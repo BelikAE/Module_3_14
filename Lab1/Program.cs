b88c8ef6e52da9b585a7c69d5f74b454bebb9eb5
BelikAE <BelikAE@HOME-PC>
2024-07-20 11:23:23 +1000

    Выдача

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,6 +10,10 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            Cat cat = new Cat("Мурка");
+            cat.ShowInfo();
+            Dog dog = new Dog("Купер");
+            dog.ShowInfo();
         }
     }
 }
